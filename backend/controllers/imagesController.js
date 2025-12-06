d9c3605b583aa36077af184842094ebb334a8861
My-Name-Hung <thanhhung11112002@gmail.com>
2025-12-06 09:54:39 +0700

    final

--- backend/controllers/imagesController.js
+++ backend/controllers/imagesController.js
@@ -73,7 +73,7 @@ const uploadImage = async (req, res) => {
         userAgent.includes("iPad") ||
         source === "web");
 
-    const fontSize = isWebIOS ? 10 : 40; // 10 for web iosauditapp, 60 for mobile app
+    const fontSize = isWebIOS ? 10 : 35; // 10 for web iosauditapp, 60 for mobile app
 
     const uploadResult = await uploadImageWithWatermark(
       req.file.buffer,
